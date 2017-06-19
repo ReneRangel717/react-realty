68a59f800ce6de15ef5d03f220006f30d5e91c77
Emil Alexandrescu <emil.alexandresku@gmail.com>
2017-06-19 19:10:39 +0300

    refactor(app): remove legacy react-google-map related files

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,4 +1,4 @@
 export Root from './Root/Root';
 export Layout from './Layout/Layout';
 export NotFound from './NotFound/NotFound';
-export MainView from './MainView/MainView';
+export DevTools from './DevTools/DevTools';
